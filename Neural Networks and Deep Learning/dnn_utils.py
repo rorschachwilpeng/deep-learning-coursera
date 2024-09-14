a0ed22a151acc4a93bf6a6dc26633f7f1737f94e
slimrorschach <slimrorschach@outlook.com>
2024-09-14 08:41:17 +0800

    第四周assignment完成

--- Neural Networks and Deep Learning/dnn_utils.py
+++ Neural Networks and Deep Learning/dnn_utils.py
@@ -1,5 +1,40 @@
 import numpy as np
 
+# def linear_forward_test_case():
+#     np.random.seed(1)
+#     A = np.random.randn(3,2)
+#     W = np.random.randn(1,3)
+#     b = np.random.randn(1,1)
+#     return A, W,
+
+def linear_activation_forward_test_case():
+    """
+    X = np.array([[-1.02387576, 1.12397796],
+ [-1.62328545, 0.64667545],
+ [-1.74314104, -0.59664964]])
+    W = np.array([[ 0.74505627, 1.97611078, -1.24412333]])
+    b = 5
+    """
+    np.random.seed(2)
+    A_prev = np.random.randn(3,2)
+    W = np.random.randn(1,3)
+    b = np.random.randn(1,1)
+    return A_prev, W, b
+
+def linear_forward_test_case():
+    np.random.seed(1)
+    """
+    X = np.array([[-1.02387576, 1.12397796],
+ [-1.62328545, 0.64667545],
+ [-1.74314104, -0.59664964]])
+    W = np.array([[ 0.74505627, 1.97611078, -1.24412333]])
+    b = np.array([[1]])
+    """
+    A = np.random.randn(3, 2)
+    W = np.random.randn(1, 3)
+    b = np.random.randn(1, 1)
+
+    return A, W, b
 
 def sigmoid(Z):
     """
